01f4125d6069316a376a65e3873851ef6c2018f6
= <seth.d.schroeder@gmail.com>
2017-10-14 11:33:35 +0100

    created RandomAppWidgetProvider

---
+++ app/src/main/java/io/github/sdsstudios/randomapppicker/RandomAppWidgetProvider.kt
@@ -0,0 +1,18 @@
+package io.github.sdsstudios.randomapppicker
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+
+/**
+ * Created by sds2001 on 14/10/17.
+ */
+
+class RandomAppWidgetProvider : AppWidgetProvider() {
+
+    override fun onUpdate(context: Context?, appWidgetManager: AppWidgetManager?, appWidgetIds: IntArray?) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
+
+
+    }
+}
